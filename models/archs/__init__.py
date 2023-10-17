de7fee8f93dec9b961d5f9d2b6aa865e260195b9
Steven <54875725+shoang22@users.noreply.github.com>
2023-10-17 11:47:17 -0400

    add vanilla transformer

--- models/archs/__init__.py
+++ models/archs/__init__.py
@@ -2,6 +2,8 @@ def define_arch(opt):
     arch_name = opt["type"]
     if arch_name == "AugmentedTransformer":
         from models.archs.augmentedtransformer import AugmentedTransformer as M
+    elif arch_name == "Transformer":
+        from models.archs.transformer import Transformer as M  
     else:
         raise NotImplementedError(f"{arch_name} is not implemented")
     
